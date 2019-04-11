0c1229bf8c9be0413827b8b1097c565e44f1d438
inkiu <hik0115@gmail.com>
2019-04-11 18:36:10 +0900

    UseCase 구조 변경

--- app/src/main/java/com/example/todo_kotlin_mvvm_dagger/clean/domain/HelloUseCase.kt
+++ app/src/main/java/com/example/todo_kotlin_mvvm_dagger/clean/domain/HelloUseCase.kt
@@ -7,8 +7,16 @@ import javax.inject.Inject
 class HelloUseCase @Inject constructor(
     private val helloRepository: IHelloRepository
 ) {
-    operator fun invoke(param: Param): Single<Hello> {
+    operator fun invoke(param: Param): Single<Result> {
         return helloRepository.getHello(param.count)
+            .map { Result.Success(it) }
+            .cast(Result::class.java)
+            .onErrorReturn { Result.Failure(it) }
     }
+
     data class Param(val count: Int)
+    sealed class Result {
+        class Success(val hello: Hello) : Result()
+        class Failure(val e: Throwable) : Result()
+    }
 }
