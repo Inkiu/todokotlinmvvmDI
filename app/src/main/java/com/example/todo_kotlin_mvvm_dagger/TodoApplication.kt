8b136b16d7837febb4f026feb5970193a24fef30
inkiu <hik0115@gmail.com>
2019-04-13 19:05:00 +0900

    DataComponent 삭제

--- app/src/main/java/com/example/todo_kotlin_mvvm_dagger/TodoApplication.kt
+++ app/src/main/java/com/example/todo_kotlin_mvvm_dagger/TodoApplication.kt
@@ -1,7 +1,5 @@
 package com.example.todo_kotlin_mvvm_dagger
 
-import com.example.todo_kotlin_mvvm_dagger.data.di.DaggerDataComponent
-import com.example.todo_kotlin_mvvm_dagger.data.di.DataComponent
 import com.example.todo_kotlin_mvvm_dagger.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -10,12 +8,7 @@ class TodoApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent
             .builder()
-//            .dataComponent(dataComponent())
             .application(this)
             .build()
-    }
-
-    private fun dataComponent(): DataComponent {
-        return DaggerDataComponent.builder().application(this).build()
     }
 }
--- app/src/main/java/com/example/todo_kotlin_mvvm_dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/todo_kotlin_mvvm_dagger/di/AppComponent.kt
@@ -2,9 +2,7 @@ package com.example.todo_kotlin_mvvm_dagger.di
 
 import android.app.Application
 import com.example.todo_kotlin_mvvm_dagger.TodoApplication
-import com.example.todo_kotlin_mvvm_dagger.data.di.DataComponent
 import com.example.todo_kotlin_mvvm_dagger.data.di.DataModule
-import com.example.todo_kotlin_mvvm_dagger.data.di.TaskRepositoryModule
 import com.example.todo_kotlin_mvvm_dagger.di.modules.ActivityBindingModule
 import com.example.todo_kotlin_mvvm_dagger.di.modules.AppModule
 import com.example.todo_kotlin_mvvm_dagger.di.modules.TasksModule
@@ -16,9 +14,6 @@ import javax.inject.Singleton
 
 @Singleton
 @Component(
-//    dependencies = [ // TODO - 이거 왜 안되는지 물어보기
-//        DataComponent::class
-//    ],
     modules = [
         AndroidSupportInjectionModule::class,
         ActivityBindingModule::class,
@@ -33,9 +28,6 @@ interface AppComponent : AndroidInjector<TodoApplication> {
 
         @BindsInstance
         fun application(application: Application): AppComponent.Builder
-
-//        fun dataComponent(dataComponent: DataComponent): AppComponent.Builder
-
         fun build(): AppComponent
     }
 }
