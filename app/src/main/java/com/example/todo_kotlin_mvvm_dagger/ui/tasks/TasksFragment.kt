70d05e0b1caadad8d8edfe15f560d7c0128176c3
inkiu <hik0115@gmail.com>
2019-04-13 15:24:03 +0900

    TasksFragment 옵션 메뉴 추가

--- app/src/main/java/com/example/todo_kotlin_mvvm_dagger/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todo_kotlin_mvvm_dagger/ui/tasks/TasksFragment.kt
@@ -1,9 +1,8 @@
 package com.example.todo_kotlin_mvvm_dagger.ui.tasks
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import androidx.lifecycle.ViewModelProviders
 import com.example.todo_kotlin_mvvm_dagger.R
 import dagger.android.support.DaggerFragment
@@ -19,6 +18,37 @@ class TasksFragment @Inject constructor(): DaggerFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.tasks_frag, container, false)
+
+        setHasOptionsMenu(true)
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.tasks_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_clear -> { } // TODO
+            R.id.menu_filter -> { showFilteringPopUpMenu() }
+            R.id.menu_refresh -> { } // TODO
+        }
+        return true
+    }
+
+    private fun showFilteringPopUpMenu() {
+        val popup = PopupMenu(requireContext(), requireActivity().findViewById(R.id.menu_filter))
+        popup.menuInflater.inflate(R.menu.filter_tasks, popup.menu)
+        popup.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+            override fun onMenuItemClick(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.active -> {} // TODO
+                    R.id.completed -> {} // TODO
+                    else -> {} // TODO
+                }
+                return true
+            }
+        })
+        popup.show()
     }
 }
