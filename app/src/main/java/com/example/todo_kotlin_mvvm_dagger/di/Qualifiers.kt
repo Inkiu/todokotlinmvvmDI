187d1561afed37992ab59c48c6d60a9857038471
inkiu <hik0115@gmail.com>
2019-05-07 07:37:23 +0900

    Scope 어노테이션 추가

---
+++ app/src/main/java/com/example/todo_kotlin_mvvm_dagger/di/Qualifiers.kt
@@ -0,0 +1,11 @@
+package com.example.todo_kotlin_mvvm_dagger.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerActivity
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerFragment
